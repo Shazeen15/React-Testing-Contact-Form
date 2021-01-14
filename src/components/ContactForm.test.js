94d5ce0aface3ae5da640d52cc1a5dc1ad5d6b2c
Shazeen Fabius <shazeenfabius@Shazeens-Mini.lan>
2021-01-14 14:34:32 -0800

    set up test file for the contact form

---
+++ src/components/ContactForm.test.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+
+import ContactForm from "./ContactForm";
+
+test("testing form", () => {
+  // arrange
+  render(<ContactForm />);
+});
